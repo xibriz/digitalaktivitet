bc4bd558dcf8403e776f94457b81af4c19b929c5
Ruben Andreassen <rubean85@gmail.com>
2019-12-18 22:45:05 +0100

    Wrong url

--- assets/js/custom-analytics.js
+++ assets/js/custom-analytics.js
@@ -8,8 +8,7 @@ window.addEventListener('load', function()
         }
         var img = document.createElement('img');
         img.setAttribute('style','display:none;');
-        img.src = 'http://localhost:5000/v1/collect-anon/'+page;
-        // img.src = 'https://admin.digitalaktivitet.no/api/v1/collect-anon/Google%20Analytics';
+        img.src = 'https://admin.digitalaktivitet.no/api/v1/collect-anon/Google%20Analytics';
         document.body.appendChild(img);    
     }
 }, false);
